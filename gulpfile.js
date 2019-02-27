0e63ed0cfb064707c5789e787f6b0900f2465bb6
Nikolay Apanasik <apn2qq@gmail.com>
2019-02-27 18:25:22 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: './build',
+  })
+
+  gulp.watch('src/scss/*.scss', ['sass'])
+  gulp.watch('build/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('sass', function() {
+  return gulp
+    .src('src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('default', ['serve'])
